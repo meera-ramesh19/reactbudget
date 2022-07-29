85d6833684c7c4bc66c12651a50a43368f804e03
meera-ramesh19 <meeraramesh19@outlook.com>
2022-07-29 08:49:03 -0400

    style issues

--- src/Pages/New.js
+++ src/Pages/New.js
@@ -3,7 +3,7 @@ import NewTransaction from '../Components/NewTransaction';
 const New = () => {
   return (
     <div style={{backgroundColor:'#96DED1', height: '100vh'}}>
-      <h2 style={{textAlign:'center',marginTop:'2rem'}}>New Transaction</h2>
+      <h2 style={{textAlign:'center'}}>New Transaction</h2>
       <NewTransaction />
     </div>
   );
