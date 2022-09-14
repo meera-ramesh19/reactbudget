51257ad31b84a165fc50df52ed495bb3a552437e
meera-ramesh19 <meeraramesh19@outlook.com>
2022-09-13 22:39:59 -0400

    changed charts

--- src/Components/charts/ChartMap.js
+++ src/Components/charts/ChartMap.js
@@ -7,8 +7,10 @@ import {
   Title,
   Tooltip,
   Legend,
+  ArcElement,
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
 
 ChartJS.register(
   CategoryScale,
@@ -16,11 +18,12 @@ ChartJS.register(
   BarElement,
   Title,
   Tooltip,
-  Legend
+  Legend,
+  ArcElement
 );
 
 const ChartMap = (props) => {
-  const { incomeArr, expenseArr, nameArr } = props;
+  const { dates, incomeArr, expenseArr, nameArr } = props;
 
   const options = {
     responsive: true,
@@ -36,25 +39,178 @@ const ChartMap = (props) => {
   };
 
   const data = {
-    labels: [nameArr],
+    labels: dates,
     datasets: [
       {
-        label: 'Income',
+        label: 'income',
         data: incomeArr,
         backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
       },
+      // {
+      //   label: 'expense',
+      //   data: expenseArr,
+      //   backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      //   borderColor: 'rgba(53, 164, 235, 1)',
+      //   borderWidth: 1,
+      // },
+    ],
+  };
+  const barColors = ['#b91d47', '#00aba9', '#2b5797', '#e8c3b9', '#1e7145'];
+
+  const data2 = {
+    labels: dates,
+    datasets: [
+      // {
+      //   label: dates,
+      //   data:nameArr,
+      //   backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      //   borderColor: 'rgba(255, 99, 132, 1)',
+      //   borderWidth: 1,
+      // },
       {
-        label: 'Expense',
+        label: 'expenses',
         data: expenseArr,
         backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        borderColor: 'rgba(53, 164, 235, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+  const config = {
+    type: 'doughnut',
+    data: data,
+    options: {
+      maintainAspectRatio: false,
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Chart.js Doughnut Chart',
+        },
+      },
+    },
+  };
+  const doughnutData = {
+    // labels:dates,
+    width: 100,
+    height: 100,
+    datasets: [
+      {
+        label: dates,
+        data: incomeArr,
+        backgroundColor: barColors,
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+  const doughnutData2 = {
+    labels: dates,
+    datasets: [
+      // {
+      //   label: dates,
+      //   data:nameArr,
+      //   backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      //   borderColor: 'rgba(255, 99, 132, 1)',
+      //   borderWidth: 1,
+      // },
+      {
+        label: 'expenses',
+        data: expenseArr,
+        backgroundColor: barColors,
+        borderColor: 'rgba(53, 164, 235, 1)',
+        borderWidth: 1,
       },
     ],
   };
   return (
-    <div>
-      <Bar options={options} data={data} />
+    <div
+      style={{
+        display: 'block',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Doughnut
+        style={{ width: '300px', height: '300px' }}
+        data={doughnutData}
+        config={config}
+      />
+      <Doughnut
+        style={{ width: '300px', height: '300px' }}
+        data={doughnutData2}
+        config={config}
+      />
+      {/* <Bar style={{width:'50%',height:'50%'}} options={options} data={data} />
+      <Bar style={{width:'50%',height:'50%'}} options={options} data={data2} /> */}
     </div>
   );
 };
 
 export default ChartMap;
+
+// import React from 'react';
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+// } from 'chart.js';
+// import { Bar } from 'react-chartjs-2';
+
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   BarElement,
+//   Title,
+//   Tooltip,
+//   Legend
+// );
+
+// const ChartMap = (props) => {
+//   const { incomeArr, expenseArr, nameArr } = props;
+
+//   const options = {
+//     responsive: true,
+//     plugins: {
+//       legend: {
+//         position: 'top',
+//       },
+//       title: {
+//         display: true,
+//         text: 'Chart.js Bar Chart',
+//       },
+//     },
+//   };
+
+//   const data = {
+//     labels: [nameArr],
+//     datasets: [
+//       {
+//         label: 'Income',
+//         data: incomeArr,
+//         backgroundColor: 'rgba(255, 99, 132, 0.5)',
+//       },
+//       {
+//         label: 'Expense',
+//         data: expenseArr,
+//         backgroundColor: 'rgba(53, 162, 235, 0.5)',
+//       },
+//     ],
+//   };
+//   return (
+//     <div>
+//       <Bar options={options} data={data} />
+//     </div>
+//   );
+// };
+
+// export default ChartMap;
--- src/Components/new/NewTransaction.js
+++ src/Components/new/NewTransaction.js
@@ -20,23 +20,6 @@ const NewTransaction = () => {
   const navigate = useNavigate();
   let { id } = useParams();
 
-  //declare states
-  const [transaction, setTransaction] = useState({
-    itemName: '',
-    amount: '',
-    userDate: '',
-    from: '',
-    category: '',
-    type: '',
-  });
-
-  // const [itemName, setItemName] = useState('');
-  // const [amount, setAmount] = useState(0);
-  // const [date, setDate] = useState('');
-  // const [from, setFrom] = useState('');
-  // const [category, setCategory] = useState('');
-  // const [type, setType] = useState('Expense');
-
   var toastMixin = Swal.mixin({
     toast: true,
     icon: 'success',
@@ -97,23 +80,86 @@ const NewTransaction = () => {
     });
   }, [makeShot]);
 
-  // const onChangeValue = (event) => {
-  //   console.log(event.target.value);
-  //   setType({  [event.target.id]: event.target.value });
-  // };
+  const sourcesData = [
+    {
+      stype: 'income',
+      categories: [
+        'Business',
+        'Investments',
+        'Extra income',
+        'Deposits',
+        'Lottery',
+        'Gifts',
+        'Salary',
+        'Savings',
+        'Rental income',
+      ],
+    },
+    {
+      stype: 'expense',
+      categories: [
+        'Bills',
+        'Car',
+        'Clothes',
+        'Travel',
+        'Food',
+        'Shopping',
+        'House',
+        'Entertainment',
+        'Phone',
+        'Pets',
+        'Other',
+      ],
+    },
+  ];
+
+  const [{ types, cat }, setData] = useState({
+    types: 'income',
+    cat: '',
+  });
+
+  const finances = sourcesData.map((source) => (
+    <option key={source.stype} value={source.stype}>
+      {source.stype}
+    </option>
+  ));
+
+  const categories = sourcesData
+    .find((item) => item.stype === types)
+    ?.categories.map((cats) => (
+      <option key={cats} value={cats}>
+        {cats}
+      </option>
+    ));
+  //declare states
+  const [transaction, setTransaction] = useState({
+    itemName: '',
+    amount: '',
+    userDate: '',
+    from: '',
+    category: '',
+    sourceType: '',
+  });
+  // handle change event of the country dropdown
+  const handlefinanceChange = (event) => {
+    console.log('types', event.target.value);
+    setData((data) => ({ cat: '', types: event.target.value }));
+  };
+
+  // handle change event of the language dropdown
+  const handleCatChange = (event) => {
+    console.log('cat', event.target.value);
+    setData((data) => ({ ...data, cat: event.target.value }));
+  };
 
   const handleTextChange = (event) => {
     console.log(event.target.value);
+
     setTransaction({
       ...transaction,
       [event.target.id]: event.target.value,
     });
   };
-
-  // const addTransaction = (transdetails) => {
-  //   let details = [...transaction, transdetails];
-  //   setTransaction(details);
-  // };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -164,7 +210,7 @@ const NewTransaction = () => {
           <input
             id='userDate'
             type='date'
-            // name='date'
+            name='userDate'
             value={transaction.userDate}
             onChange={handleTextChange}
             required
@@ -176,7 +222,7 @@ const NewTransaction = () => {
           <input
             id='from'
             type='text'
-            // name='from'
+            name='from'
             onChange={handleTextChange}
             value={transaction.from}
             required
@@ -184,28 +230,66 @@ const NewTransaction = () => {
           />
         </div>
         <div>
-          <label htmlFor='category'>Category: </label>
-          <input
-            id='category'
+          <label htmlFor='sourceType'>SourceType: </label>
+          {/* <select
+           id='sourceType' 
+           name='sourceType'
+           value={transaction.sourceType}
+           onChange={handleTextChange}>
+             {/* <option>Choose...</option>
+            <option>Income</option>
+            <option>Expense</option> 
+          </select> */}
+          {/* onChange={e => { functionOne(e); functionTwo() }} 
+             or call like this
+             onChange={this.twoCalls}
+             in twocalls
+             twoCalls = e => {
+              this.functionOne(e)
+              this.functionTwo()
+            }*/}
+
+          <select
+            id='sourceType'
             type='text'
-            // name='category'
-            value={transaction.category}
-            placeholder='food etc...'
-            onChange={handleTextChange}
-            required
-          />
+            value={transaction.sourceType}
+            onChange={(e) => {
+              handlefinanceChange(e);
+              handleTextChange(e);
+            }}
+          >
+            {finances}
+          </select>
         </div>
         <div>
-          <label htmlFor='sourcetype'>Type: </label>
-          <input
-            id='sourcetype'
-            type='text'
-            // name='sourceType'
-            value={transaction.sourcetype}
-            placeholder='Enter Income or Expense'
+          <label htmlFor='category'>Category: </label>
+
+          {/* <select
+            className='text'
             onChange={handleTextChange}
-            required
-          />
+            id='category'
+            name='category'
+            value={transaction.categ}
+           >
+           <option className='text' value='⬇️ Select an option ⬇️'>
+              {' '}
+             -- Select an option --{' '}
+             </option> 
+             {console.log(choices)}
+            //  {choices} 
+           
+          </select> */}
+          <select
+            id='category'
+            type='text'
+            value={cat}
+            onChange={(e) => {
+              handleTextChange(e);
+              handleCatChange(e);
+            }}
+          >
+            {categories}
+          </select>
         </div>
 
         <br />
@@ -226,3 +310,237 @@ const NewTransaction = () => {
 };
 
 export default NewTransaction;
+
+// https://stackoverflow.com/questions/64495308/cascade-dropdown-using-react-hooks
+// https://codesandbox.io/s/react-cascading-select-c3hji
+// https://www.cluemediator.com/cascading-dropdown-in-react
+// https://stackoverflow.com/questions/54032379/call-two-functions-within-onchange-event-in-react
+
+// import { useState, useCallback, useRef } from 'react';
+// import axios from 'axios';
+// import ReactCanvasConfetti from 'react-canvas-confetti';
+// import { useNavigate, useParams, Link } from 'react-router-dom';
+// import './NewTransaction.css';
+// import Swal from 'sweetalert2';
+
+// const API = process.env.REACT_APP_API_URL;
+
+// const canvasStyles = {
+//   position: 'fixed',
+//   pointerEvents: 'none',
+//   width: '100%',
+//   height: '100%',
+//   top: 0,
+//   left: 0,
+// };
+
+// const NewTransaction = () => {
+//   const navigate = useNavigate();
+//   let { id } = useParams();
+
+//   //declare states
+//   const [transaction, setTransaction] = useState({
+//     itemName: '',
+//     amount: '',
+//     userDate: '',
+//     from: '',
+//     category: '',
+//     type: '',
+//   });
+
+//   // const [itemName, setItemName] = useState('');
+//   // const [amount, setAmount] = useState(0);
+//   // const [date, setDate] = useState('');
+//   // const [from, setFrom] = useState('');
+//   // const [category, setCategory] = useState('');
+//   // const [type, setType] = useState('Expense');
+
+//   var toastMixin = Swal.mixin({
+//     toast: true,
+//     icon: 'success',
+//     title: 'General Title',
+//     animation: false,
+//     position: 'top-right',
+//     showConfirmButton: false,
+//     timer: 3000,
+//     timerProgressBar: true,
+//     didOpen: (toast) => {
+//       toast.addEventListener('mouseenter', Swal.stopTimer);
+//       toast.addEventListener('mouseleave', Swal.resumeTimer);
+//     },
+//   });
+
+//   const refAnimationInstance = useRef(null);
+
+//   const getInstance = useCallback((instance) => {
+//     refAnimationInstance.current = instance;
+//   }, []);
+
+//   const makeShot = useCallback((particleRatio, opts) => {
+//     refAnimationInstance.current &&
+//       refAnimationInstance.current({
+//         ...opts,
+//         origin: { y: 0.7 },
+//         particleCount: Math.floor(200 * particleRatio),
+//       });
+//   }, []);
+
+//   const fire = useCallback(() => {
+//     console.log('here');
+//     makeShot(0.25, {
+//       spread: 26,
+//       startVelocity: 55,
+//     });
+
+//     makeShot(0.2, {
+//       spread: 60,
+//     });
+
+//     makeShot(0.35, {
+//       spread: 100,
+//       decay: 0.91,
+//       scalar: 0.8,
+//     });
+
+//     makeShot(0.1, {
+//       spread: 120,
+//       startVelocity: 25,
+//       decay: 0.92,
+//       scalar: 1.2,
+//     });
+
+//     makeShot(0.1, {
+//       spread: 120,
+//       startVelocity: 45,
+//     });
+//   }, [makeShot]);
+
+//   // const onChangeValue = (event) => {
+//   //   console.log(event.target.value);
+//   //   setType({  [event.target.id]: event.target.value });
+//   // };
+
+//   const handleTextChange = (event) => {
+//     console.log(event.target.value);
+//     setTransaction({
+//       ...transaction,
+//       [event.target.id]: event.target.value,
+//     });
+//   };
+
+//   // const addTransaction = (transdetails) => {
+//   //   let details = [...transaction, transdetails];
+//   //   setTransaction(details);
+//   // };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+
+//     axios
+//       .post(`${API}/api/transactions`, transaction)
+//       .then(() => {
+//         document
+//           .querySelector('.second')
+//           .addEventListener('click', function () {
+//             toastMixin.fire({
+//               animation: true,
+//               title: 'Added a transaction',
+//             });
+//           });
+//         navigate(`/transactions`);
+//       })
+//       .catch((c) => console.error('catch', c));
+//   };
+
+//   return (
+//     <div className='add-trans'>
+//       <form onSubmit={handleSubmit}>
+//         <div>
+//           <label htmlFor='itemName'>Item Name: </label>
+//           <input
+//             id='itemName'
+//             type='text'
+//             value={transaction.itemName}
+//             onChange={handleTextChange}
+//             required
+//             placeholder='paycheck.'
+//           />
+//         </div>
+//         <div>
+//           <label htmlFor='amount'>Amount: </label>
+//           <input
+//             id='amount'
+//             type='number'
+//             required
+//             value={transaction.amount}
+//             onChange={handleTextChange}
+//             placeholder='Dollar Amount'
+//           />
+//         </div>
+//         <div>
+//           <label htmlFor='userDate'>Date: </label>
+//           <input
+//             id='userDate'
+//             type='date'
+//             // name='date'
+//             value={transaction.userDate}
+//             onChange={handleTextChange}
+//             required
+//             placeholder='date'
+//           />
+//         </div>
+//         <div>
+//           <label htmlFor='from'>From: </label>
+//           <input
+//             id='from'
+//             type='text'
+//             // name='from'
+//             onChange={handleTextChange}
+//             value={transaction.from}
+//             required
+//             placeholder='work....'
+//           />
+//         </div>
+//         <div>
+//           <label htmlFor='category'>Category: </label>
+//           <input
+//             id='category'
+//             type='text'
+//             // name='category'
+//             value={transaction.category}
+//             placeholder='food etc...'
+//             onChange={handleTextChange}
+//             required
+//           />
+//         </div>
+//         <div>
+//           <label htmlFor='sourcetype'>Type: </label>
+//           <input
+//             id='sourcetype'
+//             type='text'
+//             // name='sourceType'
+//             value={transaction.sourcetype}
+//             placeholder='Enter Income or Expense'
+//             onChange={handleTextChange}
+//             required
+//           />
+//         </div>
+
+//         <br />
+//         <div className='add-btn'>
+//           <input className='new-btns' type='submit' onClick={fire} />
+//           <button style={{ border: 'none' }} className='second'></button>
+//           <Link
+//             style={{ margin: '0 auto', textAlign: 'center' }}
+//             to={`/transactions`}
+//           >
+//             <button className='new-btns'>Cancel </button>
+//           </Link>
+//         </div>
+//         <ReactCanvasConfetti refConfetti={getInstance} style={canvasStyles} />
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default NewTransaction;
